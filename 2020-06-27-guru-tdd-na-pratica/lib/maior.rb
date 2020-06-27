e80ce60f0f6284a27366e6fd5baf22511eac83ad
Bruno Oliveira <brunoao86@gmail.com>
2020-06-27 17:29:35 -0300

    Solução #2

--- 2020-06-27-guru-tdd-na-pratica/lib/maior.rb
+++ 2020-06-27-guru-tdd-na-pratica/lib/maior.rb
@@ -1,9 +1,19 @@
 class Maior
   def retorna_maior(numeros)
-    if numeros == [1,2,3]
-      3
-    elsif numeros == [3,2,1]
-      3
+    # [1,3,2]
+
+    # o primeiro é maior
+    maior = numeros.first
+
+    # percorrer a lista de números
+    numeros.each do |numero_atual|
+      # numero atual é maior do que o maior atual
+      if numero_atual > maior
+        # então ele é o novo maior
+        maior = numero_atual
+      end
     end
+
+    maior
   end
 end
