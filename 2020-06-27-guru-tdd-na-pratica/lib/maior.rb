4ae939beaf389f840eb86f4c2f58ad308fe6be19
Bruno Oliveira <brunoao86@gmail.com>
2020-06-27 17:28:10 -0300

    Solução #1

---
+++ 2020-06-27-guru-tdd-na-pratica/lib/maior.rb
@@ -0,0 +1,9 @@
+class Maior
+  def retorna_maior(numeros)
+    if numeros == [1,2,3]
+      3
+    elsif numeros == [3,2,1]
+      3
+    end
+  end
+end
---
+++ 2020-06-27-guru-tdd-na-pratica/spec/maior_spec.rb
@@ -0,0 +1,13 @@
+require 'maior'
+
+describe Maior do
+
+  # quando a lista for [1,2,3], o maior é 3
+  it do
+    resultado = subject.retorna_maior([1,2,3])
+    expect(resultado).to eq(3)
+  end
+
+  it { expect(subject.retorna_maior([3,2,1])).to eq(3) }
+
+end
